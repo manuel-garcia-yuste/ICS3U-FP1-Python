91879401d34b325dafc8ee757956a48c9c0c93db
Ubuntu <ubuntu@ip-172-31-22-149.ec2.internal>
2019-09-20 18:13:57 +0000

    program added

---
+++ code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Created by: Manuel
+#Created on: September 2019
+#This program is the "Hello, World" program on the PyBadge
+
+def main()
+    #this function prints to the screen and console
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+    
+    while True:
+        # repeat forever, or you turn it off!
+        pass # just a place holder
+    
+if__name__ == "_main_":
+    main()
